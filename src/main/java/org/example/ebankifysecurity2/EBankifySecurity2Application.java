d65a5151326fb718c43934858b780520d132b53d
MedELHattab <136116042+MedELHattab@users.noreply.github.com>
2024-12-10 11:22:15 +0100

    Initial commit

---
+++ src/main/java/org/example/ebankifysecurity2/EBankifySecurity2Application.java
@@ -0,0 +1,13 @@
+package org.example.ebankifysecurity2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankifySecurity2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBankifySecurity2Application.class, args);
+    }
+
+}
